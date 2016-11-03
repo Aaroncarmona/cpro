75548e10a7a7ac9c7b69a503ce96a46304c2e890
AdrianCmn <carmonespi@gmail.com>
2016-11-02 19:28:52 -0600

    index - proyecto - pag estaticas
    se realizaron los templates de
    -index
    -proyecto
    -paginas estaticas

--- public_html/vista/index.php
+++ public_html/vista/index.php
@@ -1,19 +1,31 @@
 <?php
-    if (isset($_REQUEST['menu'])){
+    if (isset($_REQUEST['menu'])){//cuando le da click al aside
         if($_REQUEST['menu']=="exproy"){
             include './header.html';
             include './aside.html';
             include './barraBusqueda.html';
-            include './exproy.html';
+            include './estatico/exproy.html';
             include './footer.html';
-        }else if($_REQUEST['menu']=="equipo"){
+        }else if($_REQUEST['menu']=="galeria"){
+            include './header.html';
+            include './aside.html';
+            include './estatico/galeria.html';
+            include './footer.html';
+        }else if($_REQUEST['menu']=="cpro"){
             include './header.html';
             include './aside.html';
             include './barraBusqueda.html';
-            include './exproy.html';
+            include './estatico/cpro.html';
             include './footer.html';
         }
-    }else{
+    }else if(isset($_REQUEST['proyecto'])){//cuando se selecciona un equipo
+            if($_REQUEST['proyecto']=="1"){
+                include './header.html';
+                include './asideProyecto.html';
+                include './proyectos.html';
+                include './footer.html';
+            }
+    }else{//cuando entra el usuario
         include './header.html';
         include './aside.html';
         include './barraBusqueda.html';
